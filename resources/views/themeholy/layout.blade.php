a1b567ab6e09360c9997eca6c25454348828da57
vsphim <clemenciajohn74@gmail.com>
2026-05-20 11:47:28 +0700

    Initial commit

---
+++ resources/views/themeholy/layout.blade.php
@@ -0,0 +1,56 @@
+@extends('themes::layout')
+
+@php
+    $menu = \VsMov\Core\Models\Menu::getTree();
+@endphp
+
+@push('header')
+    <meta name="theme-color" content="#ffffff">
+    <link href="{{ asset('/themes/holy/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/themes/holy/css/fontstyle.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/themes/holy/fonts/simple-line-icons.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/themes/holy/css/styles.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/themes/holy/css/baguetteBox.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/themes/holy/css/custom.css') }}" rel="stylesheet" />
+    {{-- <style>
+        img.lazy {
+            background-image: url('http://jquery.eisbehr.de/lazy/images/loading.gif');
+            background-repeat: no-repeat;
+            background-position: 50% 50%;
+        }
+    </style> --}}
+@endpush
+
+@section('body')
+    @include('themes::themeholy.inc.nav')
+    <main class="page landing-page">
+        @yield('content')
+    </main>
+@endsection
+
+@push('scripts')
+@endpush
+
+@section('footer')
+    {!! get_theme_option('footer') !!}
+
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"
+        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <!-- cdnjs -->
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.plugins.min.js">
+    </script>
+    <script>
+        $(function() {
+            $('.lazy').lazy({
+                effect: "fadeIn",
+                effectTime: 2000,
+                threshold: 0
+            });
+        });
+    </script>
+    <script defer src="{{ asset('/themes/holy/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js" integrity="sha512-4E202psbZszCpkybu3LZGLZk1Yfi7Mv8AhTzXzyJQ7lDKdXGC0ZJ+39q+PwBjEa9QbEzFyraZ0uab0nnhJ3fNg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="{{ asset('/themes/holy/js/script.min.js') }}"></script>
+    {!! setting('site_scripts_google_analytics') !!}
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use VsMov\ThemeHoly\Controllers\ThemeHolyController;
+
+// --------------------------
+// Custom Backpack Routes
+// --------------------------
+// This route file is loaded automatically by Backpack\Base.
+// Routes you generate using Backpack\Generators will be placed here.
+
+Route::group([
+    'middleware' => array_merge(
+        (array) config('backpack.base.web_middleware', 'web'),
+    ),
+], function () {
+    Route::get('/', [ThemeHolyController::class, 'index']);
+
+    Route::get(setting('site_routes_category', '/the-loai/{category}'), [ThemeHolyController::class, 'getMovieOfCategory'])
+        ->where(['category' => '.+', 'id' => '[0-9]+'])
+        ->name('categories.movies.index');
+
+    Route::get(setting('site_routes_region', '/quoc-gia/{region}'), [ThemeHolyController::class, 'getMovieOfRegion'])
+        ->where(['region' => '.+', 'id' => '[0-9]+'])
+        ->name('regions.movies.index');
+
+    Route::get(setting('site_routes_tag', '/tu-khoa/{tag}'), [ThemeHolyController::class, 'getMovieOfTag'])
+        ->where(['tag' => '.+', 'id' => '[0-9]+'])
+        ->name('tags.movies.index');
+
+    Route::get(setting('site_routes_types', '/danh-sach/{type}'), [ThemeHolyController::class, 'getMovieOfType'])
+        ->where(['type' => '.+', 'id' => '[0-9]+'])
+        ->name('types.movies.index');
+
+    Route::get(setting('site_routes_actors', '/dien-vien/{actor}'), [ThemeHolyController::class, 'getMovieOfActor'])
+        ->where(['actor' => '.+', 'id' => '[0-9]+'])
+        ->name('actors.movies.index');
+
+    Route::get(setting('site_routes_directors', '/dao-dien/{director}'), [ThemeHolyController::class, 'getMovieOfDirector'])
+        ->where(['director' => '.+', 'id' => '[0-9]+'])
+        ->name('directors.movies.index');
+
+    Route::get(setting('site_routes_episode', '/phim/{movie}/{episode}-{id}'), [ThemeHolyController::class, 'getEpisode'])
+        ->where(['movie' => '.+', 'movie_id' => '[0-9]+', 'episode' => '.+', 'id' => '[0-9]+'])
+        ->name('episodes.show');
+
+    Route::post(sprintf('/%s/{movie}/{episode}/report', config('vsmov.routes.movie', 'phim')), [ThemeHolyController::class, 'reportEpisode'])->name('episodes.report');
+    Route::post(sprintf('/%s/{movie}/rate', config('vsmov.routes.movie', 'phim')), [ThemeHolyController::class, 'rateMovie'])->name('movie.rating');
+
+    Route::get(setting('site_routes_movie', '/phim/{movie}'), [ThemeHolyController::class, 'getMovieOverview'])
+        ->where(['movie' => '.+', 'id' => '[0-9]+'])
+        ->name('movies.show');
+
+});
